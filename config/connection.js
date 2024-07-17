16421e8e4e3fbdf3cc3c6fc093e500b913552544
Houssine <ekhay1@outlook.com>
2024-07-17 19:53:27 -0400

    added the rest of the project structure

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
+
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // requiring packages
 const express = require('express');
-const sequelize = require("./config/connection.js")
+const sequelize = require("./config/connection.js");
+
 // initializing the port and app
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // parese json
 app.use(express.json());
+
 // getting params and queries from url
 app.use(express.urlencoded({extended:true}));
 
@@ -16,5 +18,4 @@ sequelize.sync().then(()=>{
     app.listen(PORT,()=>{
         console.log(`listening at port ${PORT}`);
     })
-
 })
