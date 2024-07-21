3a3c00ec4cbe37600f2741ec91c66e9a4a8ba757
Jorge Guzman <jorgeguzman@Jorges-MacBook-Air.local>
2024-07-21 11:40:24 -0400

    page is redering

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,9 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
+router.get("/", (req, res) => {
+  res.render("login");
+});
 
 router.use('/api', apiRoutes);
 // router.use('/', homeRoutes);
