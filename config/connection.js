a07f07f86186ebc50ae058bc2220493c41cc99c8
asw <annasilverwolff@gmail.com>
2024-07-22 21:17:30 -0400

    connect to postgresql

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
 const Sequelize = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
-
     {
-        host: 'localhost',
-        dialect:'postgres'
-
+      host: "localhost",
+      dialect: "postgres",
     }
-)
+  );
+}
 
 module.exports = sequelize;
