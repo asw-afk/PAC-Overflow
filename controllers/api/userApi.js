6f4310622851bd08fbdd45b32cb22dd319885675
Houssine <ekhay1@outlook.com>
2024-07-25 21:11:19 -0400

    add logic for login logut functionality

--- controllers/api/userApi.js
+++ controllers/api/userApi.js
@@ -12,8 +12,16 @@ router.post("/signup", async (req, res) => {
     newUser.password = await bcrypt.hash(req.body.password, 8);
     // after hashing the password create and store the new user
     console.log(newUser);
-    const user = await User.create(newUser);
-    res.status(200).json({ message: "user created successfully" });
+    const CreatedUser = await User.create(newUser);
+
+    req.session.save(() => {
+      req.session.user_id = CreatedUser.id;
+      req.session.logged_in = true;
+        console.log(req.session.user_id)
+       res.status(200).json(CreatedUser);
+      
+      });
+    // res.status(200).json({ message: "user created successfully" });
   } catch (err) {
     res.status(400).json(err);
   }
@@ -51,26 +59,35 @@ router.post("/login", async (req, res) => {
       req.session.user_id = userLoginInfo.id;
       req.session.logged_in = true;
 
-        console.log(req.session.user_id)
+      console.log(req.session.logged_in);
+      console.log("This should be the loggin info");
+      console.log(userLoginInfo);
 
-       res.json({ user: userLoginInfo, logged_in: req.session.logged_in, message: "You are now logged in!" });
-       return
+        // console.log(req.session.user_id)
+
+      //  res.status(200).json({ user: userLoginInfo, logged_in: req.session.logged_in, message: "You are now logged in!" });
+      res.json({ user: userLoginInfo,message: "You are now logged in!" });
+       
+       
       });
 
   } catch (err) {
     res.status(500).json({ message: "500 internal server error" });
   }
 });
+
+
+
 //********************************Get request  */
 // full api: /api/users/
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", );
 });
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
 router.get("/post", (req, res) => {
-  res.render("post");
+  res.render("post", {logged_in: req.session.logged_in});
 });
 router.get("/homepage", (req, res) => {
   res.render("homepage");
@@ -86,5 +103,51 @@ router.post('/logout', (req, res) => {
         res.status(404).end();
     }
 });
+
+
+// login post request 
+// full url api/users/login
+// router.post("/login", async(req,res) =>{
+//     try{
+//        // assigning the user login to userLoginIngo
+//        console.log(req.body.email);
+//        const userLoginInfo = await User.findOne({where:{email: req.body.email}});
+//        console.log(userLoginInfo);
+   
+//        // checking if the user inputed data, if no info return erorr with message
+//        if(!userLoginInfo){
+//           return res.status(404).json({message: "login failed: please enter your login info"});
+//        };
+   
+//        // if the usre has all login info check if the password correct
+//        const isValidPassword = await bcrypt.compare( req.body.password, userLoginInfo.password);
+   
+//        // if the passwod does not match the user password in database 
+//        if(!isValidPassword){
+//          return  res.status(400).json({message:'password is invalid'})
+//        }
+   
+//        // if the login infor matched the one in database, allow user to login in
+//        res.status(200).json({message: "logged in successfully"})
+//     }catch(err){
+//        res.status(500).json({message:"500 internal server error"})
+   
+//     }
+//    })
+   //********************************Get request  */
+   // full api: /api/users/
+router.get("/login", (req, res)=>{
+    res.render("login")
+})
+router.get("/signup", (req, res)=>{
+    res.render("signup")
+})
+router.get("/post", (req, res)=>{
+    res.render("post")
+})
+router.get("/homepage", (req, res)=>{
+    res.render("homepage")
+})
+
 
 module.exports = router;
