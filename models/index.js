a8e822b2c5303daae832ec1e99a63f5420411cd0
Houssine <ekhay1@outlook.com>
2024-07-27 15:17:46 -0400

    updated the routes, and displayed the post in homepage, also added the logic to be able to comment and create post only if you logged in, added a get request that will return all the post with the user and comment that associated with them

--- models/index.js
+++ models/index.js
@@ -7,30 +7,43 @@ const Comment = require('./Comment')
 
 // gives Comment table access to Post_id
 // even though visually comments look like the child element of a post, the comments will be retreived from the database by their id+the id of the post they were created on
-Post.hasMany(Comment,{
-    foreignKey:'post_id',
-    onDelete: 'CASCADE',
-});
-Comment.belongsTo(Post,{
-    foreignKey:'post_id',
-});
-//gives Comments table access to user_id
-Post.hasOne(User,{
-    foreignKey:'post_id',
-    onDelete: 'CASCADE',
-});
+// Post.hasMany(Comment,{//yes
+//     foreignKey:'post_id',
+//     onDelete: 'CASCADE',
+// });
+// Comment.belongsTo(Post,{//yes
+//     foreignKey:'post_id',
+// });
 
-Post.belongsTo(User,{
-    foreignKey:'post_id',
-});
-//gives Comments table access to user_id
-Comment.hasOne(User,{
-    foreignKey:'user_id',
-    onDelete: 'CASCADE',
-});
 
-User.belongsTo(Comment,{
-    foreignKey:'user_id',
-});
 
+// //gives Comments table access to user_id
+// Post.hasOne(User,{ // no
+//     foreignKey:'user_id', // this was post_id supposed to be user_id
+//     onDelete: 'CASCADE',
+// });
+
+// Post.belongsTo(User,{ //yes
+//     foreignKey:'user_id',// this was post_id supposed to be user_id
+// });
+// //gives Comments table access to user_id
+// Comment.hasOne(User,{
+//     foreignKey:'user_id',
+//     onDelete: 'CASCADE',
+// });
+
+// User.belongsTo(Comment,{
+//     foreignKey:'user_id',  //this was user_id supposed to be comment_id
+// });
+// association betwwen user and post 
+User.hasMany(Post, { foreignKey: 'user_id' });
+Post.belongsTo(User, { foreignKey: 'user_id' });
+
+// association between post and comment 
+Post.hasMany(Comment, { foreignKey: 'post_id' });
+Comment.belongsTo(Post, { foreignKey: 'post_id' });
+
+// association between user and comment 
+User.hasMany(Comment, { foreignKey: 'user_id' });
+Comment.belongsTo(User, { foreignKey: 'user_id' });
 module.exports = { User, Post, Comment };
