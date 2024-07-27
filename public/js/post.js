6f90d5c42369155d7cda1eea998be56e5ff946cb
Houssine <ekhay1@outlook.com>
2024-07-26 22:27:17 -0400

    fixed the event delegation to target the specific buttton that was clicked

--- public/js/post.js
+++ public/js/post.js
@@ -1,12 +1,32 @@
-const comment = document.querySelector("#comment-btn");
-const comment_input_area = document.querySelector(".comment-input")
+const post_list = document.querySelector(".posts-list");
 
-comment.addEventListener("click", ()=>{
+
+// Function that will handel showing the textarea to type comment
+function showCommentInput(commentBtn){
+ // find out the exact post that the clicked button exist
+ const post = commentBtn.closest(".post");
+ // use the post to find out the exact target it area that need to change
+ const comment_input_area = post.querySelector(".comment-input");
+
+ // if the class contain hidden class remove it otherwise add it
+ if( comment_input_area.classList.contains('hidden')){
+      comment_input_area.classList.remove("hidden");
+
+  }else{
+      comment_input_area.classList.add("hidden");
+  }
+}
+
+// event handler that will handel al click in the post handlebar
+post_list.addEventListener("click", (event)=>{
     console.log("You have clicked the button");
-   if( comment_input_area.classList.contains('hidden')){
-        comment_input_area.classList.remove("hidden");
-
-    }else{
-        comment_input_area.classList.add("hidden");
+    // find the exact button that was clicked 
+    const commentBtn = event.target.closest(".comment-btn");
+   
+    // if it is found
+    if(commentBtn){
+        // pass the button to the function
+        showCommentInput(commentBtn)      
+       
     }
 })
