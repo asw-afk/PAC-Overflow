a41fdda47ade5a9705f07693d7e1078453f1f4b1
JGuzman87 <jguzman913@gmail.com>
2024-07-27 22:15:42 -0400

    modified post routes

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -2,21 +2,20 @@ const postFormHandler = async (event) => {
   event.preventDefault();
 
   const title = document.querySelector("#post-title").value.trim();
-  const game = document.querySelector("#post-game").value.trim();
   const body = document.querySelector("#post-body").value.trim();
   const votes = 0;
 
-  if (title && game && body) {
+  if (title && body) {
     const response = await fetch("/api/posts/newPost", {
       method: "POST",
-      body: JSON.stringify({ title, game, body, votes }),
+      body: JSON.stringify({ title, body, votes }),
       headers: {
         "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      document.location.replace("/api/users/post");
+      document.location.replace("/api/posts/");
     } else {
       alert("Failed to create post");
     }
